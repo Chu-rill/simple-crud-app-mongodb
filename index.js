632223b9741de00e5c83f1ea1e9d57a76f11017a
Chu-rill <churchillrimamskep@gmail.com>
2024-07-31 13:18:24 +0100

    added the mongodb connection and made some endpoints

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Product = require("./modules/product.model.js");
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from node api");
+});
+
+app.get("/api/getAllProduct", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/getAllProduct/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/api/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://churchillrimamskep:Rbq0R29l9s2IRH6d@backenddb.topi33r.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to the database");
+    app.listen(3000, () => {
+      console.log("server runing on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
